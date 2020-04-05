d1749f8fd1d54f1cdb24183f03674f073c3519b9
Joey <aileen60222@gmail.com>
2020-04-05 22:06:25 +0800

    餐點刪除功能

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,47 @@
+class ItemsController < ApplicationController
+
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def index
+    @items = Item.all
+  end
+  
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+
+    if @item.save
+      redirect_to items_path, notice: '成功新增餐點'
+    else
+      render :new, notice: '新增失敗'
+    end
+
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to items_path, notice: '成功刪除餐點'
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name,
+                                 :description,
+                                 :price,
+                                 :spec)
+  end
+
+  def record_not_found
+    render file: 'public/404.html',
+           status: 404,
+           layout: false
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  resources :items
+
+  # users
   get "/login", to: "users#login"
   post "/login", to: "users#signin"
   delete "/logout", to: "users#logout"
   get "/sign_up", to: "users#signup"
-  post "/sign_up", to: "users#register"
-  
-  
+  post "/sign_up", to: "users#register"  
 
   root "welcome#index"
 end
